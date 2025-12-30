ea79f927f273f049f648681b771374a191f9d082
lotaviods <l.otavio9099@gmail.com>
2025-12-30 16:09:44 -0300

    fix: widget_added as a string resource

--- app/src/main/java/br/com/lotaviods/listadecompras/broadcast/PinWidgetReceiver.kt
+++ app/src/main/java/br/com/lotaviods/listadecompras/broadcast/PinWidgetReceiver.kt
@@ -4,9 +4,14 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.widget.Toast
+import br.com.lotaviods.listadecompras.R
 
 class PinWidgetReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context?, intent: Intent?) {
-        Toast.makeText(context, "Widget adicionado com sucesso!", Toast.LENGTH_SHORT).show()
+        Toast.makeText(
+            context,
+            context?.getString(R.string.widget_added),
+            Toast.LENGTH_SHORT
+        ).show()
     }
 }
